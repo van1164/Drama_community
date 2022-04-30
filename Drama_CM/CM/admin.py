86f518e1b7ed7c44daa4c889ade5d594c72e5f6a
van133 <vkn123@naver.com>
2022-04-30 23:30:20 +0900

    2022/04/30

--- Drama_CM/CM/admin.py
+++ Drama_CM/CM/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Account,DRAMA
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Account)
+admin.site.register(DRAMA)
--- Drama_CM/CM/models.py
+++ Drama_CM/CM/models.py
@@ -3,18 +3,30 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+class Account(models.Model):
+    id = models.CharField(max_length=32, unique=True,primary_key=True,verbose_name='ID')
+    password = models.CharField(max_length=20, verbose_name='PASSWORD')
     created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
+            default=timezone.now,verbose_name="계정 생성일")
+    email = models.EmailField(max_length=128, unique=True, verbose_name='이메일')
+    name = models.CharField(max_length=32,verbose_name="이름")
+    watched_drama = models.TextField(default='')
     def __str__(self):
-        return self.title
+        return self.id
+    
+    class Meta:
+        db_table = 'Account'
+        verbose_name='유저'
+        verbose_name_plural='유저'
+
+#사용자가 본 드라마
+class DRAMA(models.Model):
+    drama_name = models.CharField(max_length=30,unique=True,verbose_name='드라마 제목')
+    drama_id =models.IntegerField(primary_key=True,verbose_name='드라마 아이디')
+    class Meta:
+        db_table = 'DRAMA'
+        verbose_name='DRAMA'
+        verbose_name_plural='DRAMA'
+        
+    def __str__(self):
+        return self.drama_name
